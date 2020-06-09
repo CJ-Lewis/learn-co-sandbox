c9625c1bd158dd9656d2285debd78da97cc0ae91
Christopher Lewis <cjoellewis@yahoo.com>
2020-06-09 23:39:06 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def greeting(name = "Ruby programmer")
+  puts "Hello, #{name}"
+end
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+def change_my_name
+  name = "Frank"
+  puts name.reverse
+  return name
+end
+6 + 3
---
+++ return.rb
@@ -0,0 +1,9 @@
+def print_one_plus_one
+  puts 1+1
+end
+total = 6+3
+return print_one_plus_one
+total 9+5
+
+
+
